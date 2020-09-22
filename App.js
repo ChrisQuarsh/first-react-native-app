c3062b9e16e56b2848dc61aa5d397737422d44bc
Chris Quarshie <chrisquarshie57@gmail.com>
2020-09-22 16:48:59 +0000

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./assets/landscape.jpg")} style={styles.image} />
+
+      <View style={styles.detailContainer}>
+        <Text style={styles.label}>Name</Text>
+        <Text style={[styles.label, styles.info]}>Thor</Text>
+      </View>
+
+      <View style={styles.detailContainer}>
+        <Text style={styles.label}>Email</Text>
+        <Text style={[styles.label, styles.info]}>sonofOden@mail.com</Text>
+      </View>
+
+      <View style={styles.detailContainer}>
+        <Text style={styles.label}>Gender</Text>
+        <Text style={[styles.label, styles.info]}>Male</Text>
+      </View>
+
+      <View style={styles.detailContainer}>
+        <Text style={styles.label}>Details</Text>
+        <Text style={[styles.label, styles.info]}>GodofThunder</Text>
+      </View>
+
+      <View style={styles.detailContainer}>
+        <Text style={styles.label}>Nicky</Text>
+        <Text style={[styles.label, styles.info]}>Sparkles</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +37,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal: 5,
+  },
+
+  image: {
+    height: 150,
+    width: 150,
+    marginVertical: 50,
+    alignSelf: "center",
+    borderRadius: 75,
+  },
+
+  detailContainer: {
+    flexDirection: "row",
+    marginVertical: 5,
+  },
+
+  label: {
+    borderColor: "black",
+    borderWidth: 1,
+    fontSize: 20,
+    flex: 2.5,
+    paddingHorizontal: 5,
+  },
+
+  info: {
+    flex: 7.5,
   },
 });
+
+export default App;
